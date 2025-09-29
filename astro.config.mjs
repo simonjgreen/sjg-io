4f662a3238a1e9038fcd4cda9b81d35d4c424284
Simon Green <simon@sjg.io>
2025-09-29 14:07:18 +0100

    Add sitemap integration to Astro configuration
    - Included the @astrojs/sitemap integration in astro.config.mjs to enhance site SEO capabilities.
    - Updated package.json and package-lock.json to include the new sitemap dependency version 3.6.0.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://sjg.io',
   base: '/',
   output: 'static',
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
