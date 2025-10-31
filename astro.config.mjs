c13f2e57a72a58231ca43ed3f8ad0cb77e8741ce
Simon Green <simon@sjg.io>
2025-10-31 15:43:58 +0000

    Add support for footnotes and specifying preview title/description

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import remarkGfm from 'remark-gfm';
+import rehypeFootnoteClass from './src/plugins/rehype-footnote-class.mjs';
 
 export default defineConfig({
   site: 'https://sjg.io',
   base: '/',
   output: 'static',
-  integrations: [mdx(), tailwind(), sitemap()],
+  integrations: [mdx({ 
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeFootnoteClass]
+  }), tailwind(), sitemap()],
   markdown: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeFootnoteClass],
     shikiConfig: {
       theme: 'github-dark',
       wrap: true
