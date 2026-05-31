7788a3e654cd4ca8e79538358027bf548d4b6b6f
Simon Green <simon@sjg.io>
2026-05-31 10:42:30 +0100

    Fix feed article URLs

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,7 +12,7 @@ export async function GET(context) {
       title: post.data.title,
       description: post.data.description,
       pubDate: post.data.date,
-      link: `/writing/${post.slug}/`,
+      link: `/writing/${post.id}/`,
     })),
   });
 }
